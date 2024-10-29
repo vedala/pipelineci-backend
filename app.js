ab21f6554c51f582f6a593faa33a693e04648f1e
Kalyan Vedala <svedala23@gmail.com>
2024-10-28 17:48:22 -0700

    Create get & post endpoints for /organizations with hard-coded data

--- app.js
+++ app.js
@@ -4,8 +4,23 @@ import express from "express";
 const app = express();
 const port = 3001;
 
-app.get("/", (req, res) => {
-  res.send("ok");
+app.get("/organizations", async (req, res) => {
+  res.send([
+    {
+      "id": 1,
+      "name": "org1"
+    },
+    {
+      "id": 2,
+      "name": "org2"
+    }
+  ]);
+});
+
+app.post("/organizations", async (req, res) => {
+  res.send({
+    "message": "Organization created"
+  });
 });
 
 app.listen(port, () => {
