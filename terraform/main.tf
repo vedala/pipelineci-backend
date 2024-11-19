476c3b89dbb5e6bb380547cb9107ebb8db344b0f
Kalyan Vedala <svedala23@gmail.com>
2024-11-18 17:06:22 -0800

    Hosted zone on route53 for apex domain

--- terraform/main.tf
+++ terraform/main.tf
@@ -211,11 +211,27 @@ resource "aws_lb_listener_certificate" "pipelineci_alb_certificate" {
   certificate_arn = data.aws_acm_certificate.pipelineci_certificate.arn
 }
 
-
-output "pipelineci_alb" {
-  value = aws_lb.pipelineci_alb.dns_name
-  description = "Load balancer dns_name to add to CNAME for subdomain delegation"
+data "aws_route53_zone" "pipelineciZone" {
+  name          = "${var.DOMAIN_NAME}."   // a dot appended
+  private_zone  = false
 }
+
+resource "aws_route53_record" "pipelineciRecord" {
+  zone_id = data.aws_route53_zone.pipelineciZone.zone_id
+  name          = "${var.SUBDOMAIN}.${var.DOMAIN_NAME}"
+  type    = "A"
+
+  alias {
+    name                    = aws_lb.pipelineci_alb.dns_name
+    zone_id                 = aws_lb.pipelineci_alb.zone_id
+    evaluate_target_health  = true
+  }
+}
+
+# output "pipelineci_alb" {
+#   value = aws_lb.pipelineci_alb.dns_name
+#   description = "Load balancer dns_name to add to CNAME for subdomain delegation"
+# }
 
 #
 # Database
