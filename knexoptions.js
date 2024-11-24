9fb06a539143f16d17ed021fea491b600120c0b8
Kalyan Vedala <svedala23@gmail.com>
2024-11-24 11:16:07 -0800

    Use connection components for PG database

--- knexoptions.js
+++ knexoptions.js
@@ -8,7 +8,13 @@ const knexEnvOptions = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DB_URL
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT || 5432,
+    }
   }
 }
 
