31273769dd29aaa5ed30e8e3bcd1f687ead300b8
Kalyan Vedala <svedala23@gmail.com>
2024-12-13 17:39:55 -0800

    Add migration for table 'runs'

---
+++ migrations/20241214013759_create_table_runs.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = function(knex) {
+  return knex.schema
+    .createTable('run', (table) => {
+      table.increments('id')
+      table.string('project_id')
+      table.string('owner')
+      table.string('repo')
+      table.string('sha')
+      table.string('branch')
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = function(knex) {
+  return knex.schema.dropTable('runs');
+};
