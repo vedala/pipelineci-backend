d6d064fbc4b6f139812bb958f1967f7021538ac5
Kalyan Vedala <svedala23@gmail.com>
2025-01-12 08:12:21 -0800

    Add columns tatus to runs table

---
+++ migrations/20250112160835_alter_table_runs_ad_col_status.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = function(knex) {
+  return knex.schema
+    .alterTable('runs', (table) => {
+      table.string('status')
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = function(knex) {
+  return knex.schema
+    .alterTable('runs', (table) => {
+      table.dropColumn('status')
+    });
+};
